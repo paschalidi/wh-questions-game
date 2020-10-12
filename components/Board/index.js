4c9f0b97ba9a8ba312ff9027a752b2d2a63f311c
paschalidi <paschalidi@outlook.com>
2020-10-12 16:02:58 +0100

    chore: final design

--- components/Board/index.js
+++ components/Board/index.js
@@ -4,10 +4,8 @@ import { Col } from "../Grid/Col";
 import styled from "styled-components";
 
 export const BoardStyles = styled.div`
-  padding: 8px;
+  padding: 12px;
   border: 2px solid #9336fd;
-  // display: flex;
-  // flex-wrap: wrap;
   background: #dabfff;
   border-radius: 2px;
   box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.45);
@@ -25,11 +23,21 @@ export const BoardStyles = styled.div`
     position: relative;
   }
 `;
+export const CardWhite = styled.div`
+  color: "#9336fd";
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 1px -2px,
+    rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
+  margin: 1px;
+  height: 8vw;
+  background-color: #f1f1f1;
+  border: 2px solid #d3d3d3;
+  border-radius: 2px;
+`;
 export const CardRed = styled.div`
   box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 1px -2px,
     rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
   margin: 1px;
-  height: 120px;
+  height: 8vw;
   background-color: #2b9348;
   border: 2px solid #007f5f;
   border-radius: 2px;
@@ -38,7 +46,7 @@ export const CardYellow = styled.div`
   box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 1px -2px,
     rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
   margin: 1px;
-  height: 120px;
+  height: 8vw;
   background-color: #ffff3f;
   border: 2px solid #fdf148;
   border-radius: 2px;
@@ -47,7 +55,7 @@ export const CardOrange = styled.div`
   box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 1px -2px,
     rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
   margin: 1px;
-  height: 120px;
+  height: 8vw;
   background-color: #ff9770;
   border: 2px solid #e85d04;
   border-radius: 2px;
@@ -57,28 +65,32 @@ export const Board = () => {
     <Row fullWidth>
       <Col lg={6} offset={3}>
         <BoardStyles>
-          <div className="container">
-            <div className="item">
-              <CardOrange>
-                <h1 style={{ paddingTop: 35, color: "#9336fd" }}>START</h1>
-              </CardOrange>
-            </div>
-            <div className="item">
-              <CardYellow />
-            </div>
-            <div className="item">
-              <CardRed />
-            </div>
-            <div className="item">
-              <CardRed />
-            </div>
-            <div className="item">
-              <CardYellow />
-            </div>
-            <div className="item">
+          <Row fullWidth textAlign="center">
+            <Col lg={2}>
+              <CardWhite>
+                <Row fullWidth textAlign="center" verticalAlign="middle">
+                  <Col>
+                    <h1>START</h1>
+                  </Col>
+                </Row>
+              </CardWhite>
+            </Col>
+            <Col lg={2}>
               <CardOrange />
-            </div>
-          </div>
+            </Col>
+            <Col lg={2}>
+              <CardRed />
+            </Col>
+            <Col lg={2}>
+              <CardYellow />
+            </Col>
+            <Col lg={2}>
+              <CardOrange />
+            </Col>
+            <Col lg={2}>
+              <CardRed />
+            </Col>
+          </Row>
 
           <Row fullWidth>
             <Col lg={2} offset={10}>
@@ -130,9 +142,13 @@ export const Board = () => {
               <CardOrange />
             </Col>
             <Col lg={2}>
-              <CardOrange>
-                <h1 style={{ paddingTop: 35, color: "#9336fd" }}>FINISH</h1>
-              </CardOrange>
+              <CardWhite>
+                <Row fullWidth textAlign="center" verticalAlign="middle">
+                  <Col>
+                    <h1>FINISH</h1>
+                  </Col>
+                </Row>
+              </CardWhite>
             </Col>
           </Row>
         </BoardStyles>
--- pages/game.js
+++ pages/game.js
@@ -1,17 +1,14 @@
 import { useIsAuthenticated } from "./hooks/useIsAuthenticated";
 import { RollDice } from "../components/RollDice";
-import styled from "styled-components";
 import { Board } from "../components/Board";
-export const GameStyles = styled.div`
-  height: 100vh;
-`;
+
 export default function Game({}) {
   useIsAuthenticated();
 
   return (
-    <GameStyles>
+    <>
       <RollDice />
-      <Board/>
-    </GameStyles>
+      <Board />
+    </>
   );
 }
