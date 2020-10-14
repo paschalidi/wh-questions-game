421a7422a90216bb8f793d6054a5eab4814fa6a0
paschalidi <paschalidi@outlook.com>
2020-10-14 23:45:05 +0200

    chore: globalstyles

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,9 @@
 import React, { useEffect } from 'react'
 import '../styles/globals.css'
 import { Provider } from 'react-redux'
-import { createGlobalStyle } from 'styled-components'
 import { store } from '../modules/store/redux'
 import { startLoadingFirebase } from '../modules/store/firebase/actions'
 import { NavBar } from '../components/NavBar'
-
-const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Aguafina+Script&display=swap')
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  text-decoration: none;
-  list-style: none;
-  vertical-align: baseline;
-  font-family: 'Aguafina Script', cursive;
-}
-`
 
 function MyApp({ Component, pageProps }) {
     useEffect(() => {
@@ -27,7 +12,6 @@ function MyApp({ Component, pageProps }) {
 
     return (
         <Provider store={store}>
-            <GlobalStyles />
             <NavBar />
             <Component {...pageProps} />
         </Provider>
