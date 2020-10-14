8f8e5ae5d46569972e9dd106884d434e11dc4e21
paschalidi <paschalidi@outlook.com>
2020-10-14 23:02:06 +0200

    chore: cleans

--- modules/hooks/useIsAuthenticated.js
+++ modules/hooks/useIsAuthenticated.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
 import { authStatuses } from '../store/auth/reducer'
