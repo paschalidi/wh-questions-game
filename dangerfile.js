901fe915ca96f6d7b10835e41b7099da507b6415
paschalidi <paschalidi@outlook.com>
2020-10-19 21:51:43 +0200

    chore: cleans

--- dangerfile.js
+++ dangerfile.js
@@ -1,44 +1,19 @@
-import { danger, warn, message } from 'danger'
+import { danger, warn } from 'danger'
 const fs = require('fs')
-
-// Rule: encourage all new files to be TypeScript
-// const jsAppFiles = danger.git.created_files.filter(
-//     f => f.startsWith('src/') && f.endsWith('.js')
-// )
-
-// if (jsAppFiles.length) {
-//     const listed = danger.github.utils.fileLinks(jsAppFiles)
-//     fail(`Please use <code>*.ts</code> for new files. Found: ${listed}.`)
-// }
 
 // Scan files for $FlowFixMe and encourage to fix
 const committedJsFiles = danger.git.created_files
     .concat(danger.git.modified_files)
     .filter(f => f.includes('.js'))
 
-const flowFixMeComments = ['$FlowFixMe']
+const flowFixMeComment = '$FlowFixMe'
 committedJsFiles.forEach(file => {
     const content = fs.readFileSync(file, 'utf8')
-    flowFixMeComments.forEach(comment => {
-        if (content.includes(comment)) {
-            const fileLink = danger.github.utils.fileLinks([file])
-            warn(
-                `Danger found a flow comment: \`${comment}\` in ${fileLink}.
+    if (content.includes(flowFixMeComment)) {
+        const fileLink = danger.github.utils.fileLinks([file])
+        warn(
+            `Danger found a flow comment: \`${flowFixMeComment}\` in ${fileLink}.
                 Perhaps you would like to see if this flow error can be easily fixed.`
-            )
-        }
-    })
+        )
+    }
 })
-
-message(`you the best`)
-
-// No `debugger` statements.
-// danger.git.modified_files
-//     .concat(danger.git.created_files)
-//     .filter(f => f.endsWith('.js') || f.endsWith('.ts'))
-//     .forEach(file => {
-//         const content = readFileSync(file, 'utf8')
-//         if (content.includes('debugger')) {
-//             fail(`Found a debugger statement left in by accident.`, file)
-//         }
-//     })
